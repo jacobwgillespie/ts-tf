266860e33af8f3d260f1a9b2cd3fcd3f81b81dc8
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-04-28 16:21:30 +0100

    Adjust support module path

--- packages/parse-tf-module/src/index.spec.ts
+++ packages/parse-tf-module/src/index.spec.ts
@@ -2,11 +2,11 @@ import path from 'path'
 import {parseModule} from '.'
 
 it('should correctly parse HCL module', async () => {
-  const metadata = await parseModule(path.join(__dirname, '../test/module'))
+  const metadata = await parseModule(path.join(__dirname, '../support/module'))
   expect(metadata).toMatchSnapshot()
 })
 
 it('should error when parsing a broken HCL module', async () => {
-  const metadata = parseModule(path.join(__dirname, '../test/broken-module'))
+  const metadata = parseModule(path.join(__dirname, '../support/broken-module'))
   return expect(metadata).rejects.toMatchSnapshot()
 })
