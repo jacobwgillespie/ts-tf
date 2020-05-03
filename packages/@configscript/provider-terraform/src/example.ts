24d3731c5709b6740865ba88a361b780ba65e10e
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-03 14:30:52 +0100

    Add ability to render resources to JSON objects, with references

--- packages/@configscript/provider-terraform/src/example.ts
+++ packages/@configscript/provider-terraform/src/example.ts
@@ -1,14 +1,30 @@
+export type Primitive = null | undefined | string | number | boolean | symbol | bigint
+
+type Json = string | number | boolean | null | {readonly [property: string]: Json} | readonly Json[]
+
+function isPlainObject(value: unknown): value is {readonly [property: string]: Json} {
+  return typeof value === 'object' && value !== null && value.constructor === Object
+}
+
+type StringKeyOf<T> = Extract<keyof T, string>
+
+function keysOf<T extends object>(value: T): readonly StringKeyOf<T>[] {
+  return (Object.keys(value) as unknown) as readonly StringKeyOf<T>[]
+}
+
+type AttrOrRefInner<T> = T extends object ? T | ArgumentsOrReferences<T> : T
+
 type ArgumentsOrReferences<Arguments extends object> = {
-  [k in keyof Arguments]: Arguments[k] | AttributeReference<Arguments[k]>
+  [k in keyof Arguments]: AttrOrRefInner<Arguments[k]> | AttributeReference<AttrOrRefInner<Arguments[k]>>
 }
 
 type ResourceAttributeReferences<Attributes extends object> = {
   readonly [k in keyof Attributes]: AttributeReference<Attributes[k]>
 }
 
-type Resource<Type, Arguments extends object, Attributes extends object> = {
+type Resource<Arguments extends object = object, Attributes extends object = object> = {
   readonly __kind: 'Resource'
-  readonly __type: Type
+  readonly __type: string
   readonly __name: string
   readonly __args: ArgumentsOrReferences<Arguments>
 } & ResourceAttributeReferences<Attributes>
@@ -58,12 +74,12 @@ export type IamUserAttributes = {
   readonly unique_id: string
 }
 
-export function resource<Type, Arguments extends object, Attributes extends object>(
-  type: Type,
+export function resource<Arguments extends object, Attributes extends object>(
+  type: string,
   name: string,
   args: ArgumentsOrReferences<Arguments>,
   attrRefs: ResourceAttributeReferences<Attributes>,
-): Resource<Type, Arguments, Attributes> {
+): Resource<Arguments, Attributes> {
   return {
     __kind: 'Resource',
     __type: type,
@@ -76,7 +92,7 @@ export function resource<Type, Arguments extends object, Attributes extends obje
 export function iamUser<Arguments extends IamUserArguments>(
   name: string,
   args: ArgumentsOrReferences<Arguments>,
-): Resource<'iam_user', Arguments, IamUserAttributes> {
+): Resource<Arguments, IamUserAttributes> {
   const attrRefs: ResourceAttributeReferences<IamUserAttributes> = {
     arn: attributeReference('iam_user', name, 'arn'),
     force_destroy: attributeReference('iam_user', name, 'force_destroy'),
@@ -91,6 +107,38 @@ export function iamUser<Arguments extends IamUserArguments>(
 }
 
 export const user = iamUser('my-user', {name: 'my-user'})
-export const user2 = iamUser('my-user', {name: user.name})
+export const user2 = iamUser('my-user', {name: user.name, tags: {asdf: 'true', 'other-user': user.arn}})
+
+function isAttributeReference(value: unknown): value is AttributeReference<unknown> {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    (value as AttributeReference<unknown>).__kind === 'AttributeReference'
+  )
+}
+
+function renderArgOrReference<T>(value: T | AttributeReference<T>): T | string {
+  if (isAttributeReference(value)) {
+    return `\${${value.__parentKind}.${value.__parentName}}`
+  } else if (isPlainObject(value)) {
+    return keysOf(value).reduce(
+      (renderedObject, key) => ({...renderedObject, [key]: renderArgOrReference(value[key])}),
+      value,
+    )
+  }
+  return value
+}
+
+function render(resource: Resource): object {
+  const args: ArgumentsOrReferences<Record<string, unknown>> = keysOf(resource.__args).reduce<
+    ArgumentsOrReferences<Record<string, unknown>>
+  >((renderedArgs, key) => {
+    return {...renderedArgs, [key]: renderArgOrReference(resource.__args[key])}
+  }, {})
+
+  return {[resource.__type]: {[resource.__name]: args}}
+}
 
 console.log(user2)
+console.log(JSON.stringify(render(user2), undefined, 2))
