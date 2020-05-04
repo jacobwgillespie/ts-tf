c5ec32aed096573823c9ed41a9a805e218bcccac
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-04 13:48:02 +0100

    Clean up class properties

--- packages/@configscript/provider-terraform/src/exampleClasses.ts
+++ packages/@configscript/provider-terraform/src/exampleClasses.ts
@@ -11,12 +11,19 @@ type ArgumentsOrReferences<Arguments extends object> = {
 }
 
 class AttributeReference<T> {
-  readonly __type = {} as T
+  readonly #type: T = {} as T
+  readonly #parent: Resource
 
-  constructor(public readonly parent: Resource, public readonly name: string) {}
+  constructor(parent: Resource, public readonly name: string) {
+    this.#parent = parent
+
+    // This does nothing, but it's required to make TypeScript happy
+    // eslint-disable-next-line functional/no-expression-statement
+    this.#type
+  }
 
   render(): string {
-    return `\${${this.parent.__type}.${this.parent.__name}.${this.name}}`
+    return `\${${this.#parent.__kind}.${this.#parent.__name}.${this.name}}`
   }
 }
 
@@ -44,15 +51,10 @@ function unwrap<T>(value: T): unknown {
 }
 
 abstract class Resource<Arguments extends object = {}, Attributes extends object = {}> {
-  get __kind(): 'Resource' {
-    return 'Resource'
-  }
+  abstract get __kind(): string
+  abstract get __provider(): string
 
-  public abstract readonly __type: string = '__type'
-
-  constructor(public readonly __name: string, public readonly __args: ArgumentsOrReferences<Arguments>) {
-    Object.seal(this)
-  }
+  constructor(public readonly __name: string, public readonly __args: ArgumentsOrReferences<Arguments>) {}
 
   render(): object {
     const args: ArgumentsOrReferences<Record<string, unknown>> = keysOf(this.__args).reduce<
@@ -64,11 +66,20 @@ abstract class Resource<Arguments extends object = {}, Attributes extends object
 
     console.log(args)
 
-    return {[this.__type]: {[this.__name]: args}}
+    return {[this.__kind]: {[this.__name]: args}}
   }
 
   protected __attr<K extends StringKeyOf<Attributes>>(key: K): AttributeReference<Attributes[K]> {
     return new AttributeReference<Attributes[K]>(this, key)
+  }
+}
+
+abstract class TerraformResource<Arguments extends object = {}, Attributes extends object = {}> extends Resource<
+  Arguments,
+  Attributes
+> {
+  get __provider(): 'terraform' {
+    return 'terraform'
   }
 }
 
@@ -94,38 +105,24 @@ export type IamUserAttributes = {
   readonly unique_id: string
 }
 
-class IamUser extends Resource<IamUserArguments, IamUserAttributes> {
-  readonly __type = 'iam_user'
+class IamUser extends TerraformResource<IamUserArguments, IamUserAttributes> {
+  get __kind(): 'iam_user' {
+    return 'iam_user'
+  }
 
   constructor(name: string, args: ArgumentsOrReferences<IamUserArguments>) {
     super(name, args)
     Object.freeze(this)
   }
 
-  get arn() {
-    return this.__attr('arn')
-  }
-  get force_destroy() {
-    return this.__attr('force_destroy')
-  }
-  get id() {
-    return this.__attr('id')
-  }
-  get name() {
-    return this.__attr('name')
-  }
-  get path() {
-    return this.__attr('path')
-  }
-  get permissions_boundary() {
-    return this.__attr('permissions_boundary')
-  }
-  get tags() {
-    return this.__attr('tags')
-  }
-  get unique_id() {
-    return this.__attr('unique_id')
-  }
+  readonly arn = this.__attr('arn')
+  readonly force_destroy = this.__attr('force_destroy')
+  readonly id = this.__attr('id')
+  readonly name = this.__attr('name')
+  readonly path = this.__attr('path')
+  readonly permissions_boundary = this.__attr('permissions_boundary')
+  readonly tags = this.__attr('tags')
+  readonly unique_id = this.__attr('unique_id')
 }
 
 export const user = new IamUser('my-user', {name: 'my-user'})
