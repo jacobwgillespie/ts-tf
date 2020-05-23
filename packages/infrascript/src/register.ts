259100e0a2bcc5f4f9c0e81a42f801a2bc7d65c1
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-23 01:42:34 +0100

    Remove unnecessary ts-ignore comment

--- packages/infrascript/src/register.ts
+++ packages/infrascript/src/register.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment,@typescript-eslint/prefer-ts-expect-error
-// @ts-ignore - no types available
 import * as pirates from 'pirates'
 
 import {register as tsNodeRegister, Register} from 'ts-node'
