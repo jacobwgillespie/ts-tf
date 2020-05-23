7a15773d658c1ba3a03754b32758596e5bfd50b4
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-23 02:10:42 +0100

    Remove sucrase

--- packages/infrascript/src/register.ts
+++ packages/infrascript/src/register.ts
@@ -1,29 +1,24 @@
-import * as pirates from 'pirates'
+// import * as pirates from 'pirates'
 
 import {register as tsNodeRegister, Register} from 'ts-node'
 import * as ts from 'typescript'
 import {defaultCompilerOptions} from './typescript'
-import {Options, transform} from 'sucrase'
 
-function addHook(extension: string, options: Options): void {
-  pirates.addHook(
-    (code: string, filePath: string): string => {
-      const {code: transformedCode, sourceMap} = transform(code, {
-        ...options,
-        sourceMapOptions: {compiledFilename: filePath},
-        filePath,
-      })
-      const mapBase64 = Buffer.from(JSON.stringify(sourceMap)).toString('base64')
-      const suffix = `//# sourceMappingURL=data:application/json;charset=utf-8;base64,${mapBase64}`
-      return `${transformedCode}\n${suffix}`
-    },
-    {exts: [extension]},
-  )
-}
-
-export function registerSucrase(): void {
-  addHook('.ts', {transforms: ['imports', 'typescript']})
-}
+// function addHook(extension: string, options: Options): void {
+//   pirates.addHook(
+//     (code: string, filePath: string): string => {
+//       const {code: transformedCode, sourceMap} = transform(code, {
+//         ...options,
+//         sourceMapOptions: {compiledFilename: filePath},
+//         filePath,
+//       })
+//       const mapBase64 = Buffer.from(JSON.stringify(sourceMap)).toString('base64')
+//       const suffix = `//# sourceMappingURL=data:application/json;charset=utf-8;base64,${mapBase64}`
+//       return `${transformedCode}\n${suffix}`
+//     },
+//     {exts: [extension]},
+//   )
+// }
 
 export function registerTSNode(compilerOptions: ts.CompilerOptions = defaultCompilerOptions): Register {
   return tsNodeRegister({
