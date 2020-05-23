e9265722fa94b7498676f2db68d4c2f6c7f45d5d
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-23 16:23:21 +0100

    Add merge and copy utilities for Graph

--- packages/@infrascript/core/src/Graph.ts
+++ packages/@infrascript/core/src/Graph.ts
@@ -21,4 +21,34 @@ export class Graph<Node> {
   edges(): ReadonlyMap<Node, ReadonlySet<Node>> {
     return this.#edges
   }
+
+  static merge<T>(a: Graph<T>, b: Graph<T>): Graph<T> {
+    const combined = new Graph<T>()
+
+    for (const node of a.#nodes) {
+      combined.addNode(node)
+    }
+
+    for (const node of b.#nodes) {
+      combined.addNode(node)
+    }
+
+    for (const [node, edges] of a.#edges) {
+      for (const edge of edges) {
+        combined.addEdge(node, edge)
+      }
+    }
+
+    for (const [node, edges] of b.#edges) {
+      for (const edge of edges) {
+        combined.addEdge(node, edge)
+      }
+    }
+
+    return combined
+  }
+
+  static copy<T>(a: Graph<T>): Graph<T> {
+    return Graph.merge(a, new Graph())
+  }
 }
