94afc343110f19805be913c7dc236a4d267e4188
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-23 16:41:39 +0100

    Update example with a new namespace

--- packages/@infrascript/core/src/example.ts
+++ packages/@infrascript/core/src/example.ts
@@ -2,13 +2,27 @@ import {Context} from './context'
 import {Namespace} from './Namespace'
 import {ExampleResource1, ExampleResource2} from './Resource'
 
-Context.for('ctx', {prefix: '', namespace: Namespace.root})
+async function run() {
+  Context.for('ctx', {prefix: '', namespace: Namespace.root})
 
-const r = new ExampleResource1('resource', {prop1: 1, prop2: '2'})
-console.log(r)
+  const r1 = new ExampleResource1('resource', {prop1: 1, prop2: '2'})
+  console.log(r1)
 
-const r2 = new ExampleResource2('resource2', {prop1: 1, prop2: r.prop2})
-console.log(r2)
+  const r2 = new ExampleResource2('resource2', {prop1: 1, prop2: r1.prop2})
+  console.log(r2)
 
-console.log('nodes', Namespace.root.graph.nodes())
-console.log('edges', Namespace.root.graph.edges())
+  const customNamespace = new Namespace('custom')
+  await customNamespace.withNamespace(() => {
+    const r3 = new ExampleResource1('resource', {prop1: 1, prop2: r1.prop2})
+    console.log(r3)
+  })
+
+  console.log('nodes', Namespace.root.graph.nodes())
+  console.log('edges', Namespace.root.graph.edges())
+  console.log(Namespace.root.graph.dot((n) => n.urn))
+}
+
+run().catch((error: Error) => {
+  console.log(error.stack)
+  process.exit(1)
+})
