12437131fc5dc7ff15aaeca8ffb2228a4af5561b
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-24 02:56:05 +0100

    Add formatted error output with @babel/code-frame

--- packages/@infrascript/core/src/example.ts
+++ packages/@infrascript/core/src/example.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import 'source-map-support/register'
 
+import {codeFrameColumns} from '@babel/code-frame'
+import {parse as parseErrorStack} from 'error-stack-parser'
+import fs from 'fs-extra'
 import {Namespace} from './Namespace'
 import {ExampleResource1, ExampleResource2, globalRoot} from './Resource'
 
@@ -9,6 +13,7 @@ async function run() {
 
   const r2 = new ExampleResource2('resource2', {prop1: 1, prop2: r1.prop2})
   console.log(r2)
+  new ExampleResource2('resource2', {prop1: 1, prop2: r1.prop2})
 
   const customNamespace = new Namespace('custom')
   await customNamespace.$asParent(() => {
@@ -22,7 +27,31 @@ async function run() {
   console.log(graph.dot((n) => n.$urn))
 }
 
-run().catch((error: Error) => {
-  console.log(error.stack)
+function indent(count: number, source: string): string {
+  return source.replace(/^(?!\s*$)/gm, ' '.repeat(count))
+}
+
+run().catch(async (error: Error) => {
+  console.error(`ERROR: ${error.message}`)
+  const stack = parseErrorStack(error)
+  for (const line of stack) {
+    if (line.fileName?.startsWith(process.cwd())) {
+      const source = (await fs.readFile(line.fileName)).toString('utf8')
+      console.error(indent(2, `${line.source!.trim()}:\n`))
+      console.error(
+        indent(
+          4,
+          codeFrameColumns(
+            source,
+            {start: {line: line.lineNumber!, column: line.columnNumber!}},
+            {highlightCode: true},
+          ),
+        ),
+      )
+      console.error('\n')
+    } else {
+      console.log(line.source)
+    }
+  }
   process.exit(1)
 })
