ee59e1222dd213d4afc9deb2f3b34477bad97dcb
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-25 23:45:18 +0100

    Fix pretty-printing errors

--- packages/@infrascript/ui/src/errors.ts
+++ packages/@infrascript/ui/src/errors.ts
@@ -1,27 +1,35 @@
 import {codeFrameColumns} from '@babel/code-frame'
+import chalk from 'chalk'
 import {parse as parseErrorStack} from 'error-stack-parser'
 import * as fs from 'fs'
 import * as path from 'path'
 import {indent} from './utilities'
 
 export function prettyPrintError(error: Error): void {
-  console.error(`ERROR: ${error.message}`)
+  console.error(chalk.red(`ERROR: ${error.message}\n`))
   const stack = parseErrorStack(error)
   for (const line of stack) {
     const {fileName, source, lineNumber, columnNumber} = line
 
     const resolvedPath = fileName !== undefined ? path.resolve(fileName) : undefined
-    const localPath = resolvedPath?.startsWith(process.cwd()) ? resolvedPath : undefined
+    const isLocalSourceFile =
+      (resolvedPath?.startsWith(process.cwd()) && !resolvedPath.includes('/node_modules/')) ?? false
 
-    if (localPath !== undefined && source !== undefined && lineNumber !== undefined && columnNumber !== undefined) {
-      const source = fs.readFileSync(localPath).toString('utf8')
-      console.error(indent(2, `${source.trim()}:\n`))
+    if (
+      isLocalSourceFile &&
+      resolvedPath !== undefined &&
+      source !== undefined &&
+      lineNumber !== undefined &&
+      columnNumber !== undefined
+    ) {
+      console.error(indent(2, chalk.gray(`${source.trim()}:\n`)))
+      const code = fs.readFileSync(resolvedPath).toString('utf8')
       console.error(
-        indent(4, codeFrameColumns(source, {start: {line: lineNumber, column: columnNumber}}, {highlightCode: true})),
+        indent(4, codeFrameColumns(code, {start: {line: lineNumber, column: columnNumber}}, {highlightCode: true})),
       )
       console.error('\n')
     } else {
-      console.log(line.source)
+      console.error(chalk.gray(line.source))
     }
   }
 }
