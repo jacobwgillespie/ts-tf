1a6e5fd25025e9d97822e8105a3ceee607d0f6e5
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-27 11:38:03 +0100

    Upgrade dependencies

--- packages/@infrascript/driver-terraform/src/TerraformResource.ts
+++ packages/@infrascript/driver-terraform/src/TerraformResource.ts
@@ -1,3 +1,4 @@
 import {PropInputObject, Resource} from '@infrascript/core'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class TerraformResource<T extends PropInputObject = any> extends Resource<T> {}
