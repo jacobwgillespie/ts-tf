2bcc69688dc3248f3deaff3564526036e3c9a5c5
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-06-01 13:50:58 +0100

    Fix build, linting

--- packages/@infrascript/reconciliation/src/Loop.ts
+++ packages/@infrascript/reconciliation/src/Loop.ts
@@ -1,13 +1,14 @@
-import { Scheduler } from "./Scheduler";
-import { Plan } from "./Plan";
+import {Scheduler} from './Scheduler'
+import {Plan} from './Plan'
 
 export async function reconcile(plan: Plan): Promise<void> {
-    const scheduler = new Scheduler()
-    while(true) {
-        console.log("starting reconciliation loop")
-        const error = scheduler.executePlan(plan)
-        if (error) {
-            console.warn(error)
-        }
+  const scheduler = new Scheduler()
+  // eslint-disable-next-line no-constant-condition,@typescript-eslint/no-unnecessary-condition
+  while (true) {
+    console.log('starting reconciliation loop')
+    const error = scheduler.executePlan(plan)
+    if (error) {
+      console.warn(error)
     }
+  }
 }
--- packages/@infrascript/reconciliation/src/example.ts
+++ packages/@infrascript/reconciliation/src/example.ts
@@ -1,12 +1,12 @@
-import { GenericPlan } from "./Plan"
-import { reconcile } from "./Loop"
+import {GenericPlan} from './Plan'
+import {reconcile} from './Loop'
 
 async function run(): Promise<void> {
-    const plan = new GenericPlan()
-    await reconcile(plan)
+  const plan = new GenericPlan()
+  await reconcile(plan)
 }
 
 run().catch((error: Error) => {
-    console.log(error.stack)
-    process.exit(1)
+  console.log(error.stack)
+  process.exit(1)
 })
